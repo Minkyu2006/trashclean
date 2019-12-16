4600f7b5c624c26d7f582abd609a3ba6c107642f
Minkyu3 <gkstls2006@naver.com>
2019-12-16 14:32:11 +0900

    모든페이지 alert적용

--- src/main/resources/static/assets/js/new/common.js
+++ src/main/resources/static/assets/js/new/common.js
@@ -331,8 +331,8 @@ function nvl(str, defaultStr){
 //Ajax 호출시 에러가났을경우의 메세지 함수
 function ajaxErrorMsg(request) {
 	if (request.status == "403") {
-		alert("로그인 정보가 만료되었거나 권한이 없습니다. 다시 로그인 하세요");
+		alertCancel("로그인 정보가 만료되었거나 권한이 없습니다. 다시 로그인 하세요");
 	} else {
-		alert("code:" + request.status + "\n" + "message:" + request.responseText);
+		alertCancel("로그인 정보가 만료되었거나 권한이 없습니다. 다시 로그인 하세요");
 	}
 }
