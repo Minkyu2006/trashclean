6e5ce2461f7f399bf185247962e7b19b63aa5f48
CIS-Mac_BW <kr.chis@gmail.com>
2019-12-26 14:15:30 +0900

    안쓰는 import 정리

--- src/main/java/kr/co/broadwave/aci/dashboard/DashboardRestController.java
+++ src/main/java/kr/co/broadwave/aci/dashboard/DashboardRestController.java
@@ -1,11 +1,8 @@
 package kr.co.broadwave.aci.dashboard;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.co.broadwave.aci.bscodes.CodeType;
 import kr.co.broadwave.aci.common.AjaxResponse;
 import kr.co.broadwave.aci.common.CommonUtils;
-import kr.co.broadwave.aci.equipment.Equipment;
 import kr.co.broadwave.aci.equipment.EquipmentDto;
 import kr.co.broadwave.aci.equipment.EquipmentService;
 import kr.co.broadwave.aci.mastercode.MasterCode;
@@ -17,14 +14,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.rmi.CORBA.ValueHandler;
 import java.io.IOException;
 import java.util.*;
 
