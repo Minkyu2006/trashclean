bb30c871f0ddd745e91380d5bdb918b7b82cd5c0
CIS-Mac_BW <kr.chis@gmail.com>
2020-02-04 09:30:25 +0900

    Iot Service Shadow 명령추가  - 액추에에터리셋 setActuatorReset  - 수거시작  setCollectStart  - 수거종료 setCollectEnd  - LED 점멸 setLightFlicker

--- src/main/java/kr/co/broadwave/aci/awsiot/ACIAWSIoTDeviceService.java
+++ src/main/java/kr/co/broadwave/aci/awsiot/ACIAWSIoTDeviceService.java
@@ -56,13 +56,42 @@ public class ACIAWSIoTDeviceService {
 
     }
 
-    //Shadow Isolarbin DataReq(IoT)
+    //Shadow Isolarbin DataReq(IoT) -> param : 디바이스 아이디, 타임스탬프
     public void setDataRequest(String deviceid,String ts) throws Exception {
         log.info("장비 Shadow 데이터 요청 :'" + deviceid +"'");
         //device 상태가져오기
         aciIoTService.shadowNonblockingSend(deviceid,"datareq",ts);
 
     }
+    //Shadow Isolarbin 액츄에이터리셋(IoT) -> param : 디바이스 아이디, 타임스탬프
+    public void setActuatorReset(String deviceid,String ts) throws Exception {
+        log.info("장비 Shadow 액추에이터리셋 요청 :'" + deviceid +"'");
+        //device 상태가져오기
+        aciIoTService.shadowNonblockingSend(deviceid,"v_actuatorreset",ts);
+
+    }
+    //Shadow Isolarbin 수거관리시작 (IoT) -> param :  디바이스 아이디, 수거관리번호
+    public void setCollectStart(String deviceid,String ctCode) throws Exception {
+        log.info("장비 Shadow 수거관리시작 요청 :'" + deviceid +"'");
+        //device 상태가져오기
+        aciIoTService.shadowNonblockingSend(deviceid,"collect_start",ctCode);
+
+    }
+    //Shadow Isolarbin 수거관리종료 (IoT) -> param :  디바이스 아이디, 수거관리번호
+    public void setCollectEnd(String deviceid,String ctCode) throws Exception {
+        log.info("장비 Shadow 수거관리종료 요청 :'" + deviceid +"'");
+        //device 상태가져오기
+        aciIoTService.shadowNonblockingSend(deviceid,"collect_end",ctCode);
+
+    }
+    //Shadow Isolarbin 수거관리종료 (IoT) -> param :  디바이스 아이디, 타임스탬프
+    public void setLightFlicker(String deviceid,String ts) throws Exception {
+        log.info("장비 Shadow LED점멸 요청 :'" + deviceid +"'");
+        //device 상태가져오기
+        aciIoTService.shadowNonblockingSend(deviceid,"light_flicker",ts);
+
+    }
+
 
 
 }
