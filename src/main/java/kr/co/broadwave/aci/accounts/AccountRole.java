36a31fc1984590952abe1d4220620b14020dec9e
Minkyu3 <gkhtls2006@broadwave.co.kr>
2020-02-06 14:03:47 +0900

    기존 어드민 -> 시스템관리자 추가 서브어드민 -> 관리자 로변경

--- src/main/java/kr/co/broadwave/aci/accounts/AccountRole.java
+++ src/main/java/kr/co/broadwave/aci/accounts/AccountRole.java
@@ -8,8 +8,8 @@ package kr.co.broadwave.aci.accounts;
  */
 public enum AccountRole {
     ROLE_USER("ROLE_USER", "운영사"),
-    ROLE_ADMIN("ROLE_ADMIN", "관리자"),
-    ROLE_SUBADMIN("ROLE_SUBADMIN", "서브관리자"),
+    ROLE_ADMIN("ROLE_ADMIN", "시스템관리자"),
+    ROLE_SUBADMIN("ROLE_SUBADMIN", "관리자"),
     ROLE_COLLECTOR("ROLE_COLLECTOR", "수거원")
     ;
 
