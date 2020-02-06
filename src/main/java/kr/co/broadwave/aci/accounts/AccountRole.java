f7bf2e7c538707c9b450979a46ea00c91c3e413a
Minkyu3 <gkhtls2006@broadwave.co.kr>
2020-02-06 13:58:45 +0900

    서브어드민 권한추가

--- src/main/java/kr/co/broadwave/aci/accounts/AccountRole.java
+++ src/main/java/kr/co/broadwave/aci/accounts/AccountRole.java
@@ -9,6 +9,7 @@ package kr.co.broadwave.aci.accounts;
 public enum AccountRole {
     ROLE_USER("ROLE_USER", "운영사"),
     ROLE_ADMIN("ROLE_ADMIN", "관리자"),
+    ROLE_SUBADMIN("ROLE_SUBADMIN", "서브관리자"),
     ROLE_COLLECTOR("ROLE_COLLECTOR", "수거원")
     ;
 
--- src/main/java/kr/co/broadwave/aci/dashboard/DashboardRestController.java
+++ src/main/java/kr/co/broadwave/aci/dashboard/DashboardRestController.java
@@ -492,11 +492,11 @@ public class DashboardRestController {
 //        log.info("deviceIdNames : "+deviceIdNames);
 //        log.info("gps_laDatas2 : "+gps_laDatas2);
 //        log.info("gps_loDatas2 : "+gps_loDatas2);
-//        log.info("mapDataColumns : "+mapDataColumns);
+        //log.info("mapDataColumns : "+mapDataColumns);
 
 
 
-//        log.info("오름차순 안된 streetMeter : "+streetMeter);
+//        System.out.println("오름차순 안된 streetMeter : "+streetMeter);
 //        // 거리(Double) 오름차순
 //        streetMeter.sort(new Comparator<HashMap<String, Object>>() {
 //            @Override
@@ -506,8 +506,7 @@ public class DashboardRestController {
 //                return streetage1.compareTo(streetage2);
 //            }
 //        });
-//
-//        log.info("오름차순 된 streetMeter : "+streetMeter);
+//        System.out.println("오름차순 된 streetMeter : "+streetMeter);
 //        data.put("streetMeter", streetMeter);
 
 
@@ -528,17 +527,17 @@ public class DashboardRestController {
     }
 
     // 본부와 솔라빈 간의 거리
-    public static double haversine(double lat1, double lon1, double lat2, double lon2) {
-        double R = 6372.8; // In kilometers
-        double dLat = Math.toRadians(lat2 - lat1);
-        double dLon = Math.toRadians(lon2 - lon1);
-        lat1 = Math.toRadians(lat1);
-        lat2 = Math.toRadians(lat2);
-
-        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
-        double c = 2 * Math.asin(Math.sqrt(a));
-        return R * c;
-    }
+//    public static double haversine(double lat1, double lon1, double lat2, double lon2) {
+//        double R = 6372.8; // In kilometers
+//        double dLat = Math.toRadians(lat2 - lat1);
+//        double dLon = Math.toRadians(lon2 - lon1);
+//        lat1 = Math.toRadians(lat1);
+//        lat2 = Math.toRadians(lat2);
+//
+//        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+//        double c = 2 * Math.asin(Math.sqrt(a));
+//        return R * c;
+//    }
 
 
     // 원차트 새로고침
