c198c236f368403e067574db5361be63e790612c
Minkyu3 <gkhtls2006@broadwave.co.kr>
2020-02-28 10:33:24 +0900

    장비정보 처리페이지

--- src/main/java/kr/co/broadwave/aci/controller/DeviceStatsController.java
+++ src/main/java/kr/co/broadwave/aci/controller/DeviceStatsController.java
@@ -1,13 +1,18 @@
 package kr.co.broadwave.aci.controller;
 
+import kr.co.broadwave.aci.awsiot.ACIAWSLambdaService;
 import kr.co.broadwave.aci.bscodes.CodeType;
+import kr.co.broadwave.aci.dashboard.DashboardService;
 import kr.co.broadwave.aci.mastercode.MasterCodeDto;
 import kr.co.broadwave.aci.mastercode.MasterCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -21,10 +26,15 @@ import java.util.List;
 public class DeviceStatsController {
 
     private final MasterCodeService masterCodeService;
-
+    private final ACIAWSLambdaService aciawsLambdaService;
+    private final DashboardService dashboardService;
     @Autowired
-    public DeviceStatsController(MasterCodeService masterCodeService) {
+    public DeviceStatsController(MasterCodeService masterCodeService,
+                                 DashboardService dashboardService,
+                                 ACIAWSLambdaService aciawsLambdaService) {
         this.masterCodeService = masterCodeService;
+        this.dashboardService = dashboardService;
+        this.aciawsLambdaService = aciawsLambdaService;
     }
 
     //장비등록
@@ -68,4 +78,41 @@ public class DeviceStatsController {
 
         return "devicestats/devicebasevalue";
     }
+
+    @RequestMapping("devicesearchinfo")
+    public String devicesearchinfo(){
+        return "devicestats/devicesearchinfo";
+    }
+
+    @RequestMapping("deviceinfo/{emNumber}")
+    public String deviceinfo(Model model,@PathVariable String emNumber){
+
+        // Json String emNumber 만들기
+        String stateEmNumber = '"'+emNumber+'"';
+        List<String> emNumbers = new ArrayList<>();
+        emNumbers.add(stateEmNumber);
+        HashMap<String,List<String>> deviceids = new HashMap<>();
+        deviceids.put('"'+"deviceids"+'"',emNumbers);
+        String aswDeviceid = deviceids.toString().replace("=",":").replace(" ","");
+
+        HashMap<String, HashMap<String,String>> deviceInfoMap = aciawsLambdaService.getDeviceInfo(emNumber);
+        HashMap<String, HashMap<String,String>> resData = dashboardService.getDeviceLastestState(aswDeviceid);
+
+        System.out.println("emNumber : "+emNumber);
+        System.out.println("deviceInfoMap : "+deviceInfoMap);
+        System.out.println("resData : "+resData);
+
+        HashMap<String,String> deviceInfo = deviceInfoMap.get("data");
+//        HashMap<String,String> deviceAws = resData.get("data");
+        System.out.println("deviceInfo : "+deviceInfo);
+        //System.out.println("deviceAws : "+deviceAws);
+
+        model.addAttribute("deviceid", deviceInfo.get("deviceid"));
+//        model.addAttribute("equipdCountrys", equipdCountrys);
+//        model.addAttribute("equipdTypes", equipdTypes);
+//        model.addAttribute("equipdCountrys", equipdCountrys);
+
+        return "devicestats/deviceinfo";
+    }
+
 }
